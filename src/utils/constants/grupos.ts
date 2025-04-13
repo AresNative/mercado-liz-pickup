1882b019441dbe91be229f633c945a55f3f55f05
Juan José Eusebio <juanerpgr@gmail.com>
2025-04-12 17:28:59 -0700

    feat: agregar constantes de categorías y grupos en el sistema

---
+++ src/utils/constants/grupos.ts
@@ -0,0 +1,18 @@
+const categorias = [
+  "FRUTAS Y VERDURAS",
+  "FARMACIA",
+  "ROPA",
+  "VARIEDADES",
+  "PERECEDEROS",
+  "Varios",
+  "PASTELERIA", // Nueva categoría
+  "PANADERIA", // Nueva categoría
+  "VENTAS DEPARTAMENTALES", // Nueva categoría
+  "ALIMENTOS PREPARADOS", // Nueva categoría
+  "CHECK OUT", // Nueva categoría
+  "MERCANCIAS GENERALES",
+  "TORTILLERIA",
+  "GRANELES",
+  "CREMAS",
+  "ABARROTES",
+];
