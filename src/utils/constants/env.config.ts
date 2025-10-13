756da5e0de355c2d76a7216e82610b0d5fae33f7
Juan José Eusebio <juanerpgr@gmail.com>
2025-10-12 17:24:45 -0700

    feat: agregar soporte para SignalR y actualizar configuración de entorno

--- src/utils/constants/env.config.ts
+++ src/utils/constants/env.config.ts
@@ -2,6 +2,7 @@
 type EnvConfigType = {
   api: any;
   api_int: any;
+  hubs: any;
   mode: string;
   itemsPerPage: number;
 };
@@ -21,13 +22,20 @@ export const EnvConfig = (): EnvConfigType => {
     mode === "production"
       ? process.env.REACT_APP_API_URL ?? "http://localhost:5230/api/"
       : "http://localhost:5230/api/";
+
   const api_int = process.env.REACT_PUBLIC_API_URL_INT;
+
+  const hubs =
+    mode === "production"
+      ? process.env.REACT_APP_HUB_URL ?? "http://localhost:5230/"
+      : "http://localhost:5230/";
 
   const itemsPerPage = parseInt(process.env.ITEMS_PER_PAGE || "10", 10); // Fallback a 10 si no está definido
 
   return {
     api,
     api_int,
+    hubs,
     mode,
     itemsPerPage,
   };
