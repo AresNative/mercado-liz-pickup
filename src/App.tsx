4185769a49f25f56796953ccaeee89ae6db91240
Juan José Eusebio <juanerpgr@gmail.com>
2026-08-03 14:12:38 -0700

    fix: eliminar IonReactRouter anidado y simplificar la estructura de rutas en App

--- src/App.tsx
+++ src/App.tsx
@@ -18,9 +18,7 @@ import '@ionic/react/css/palettes/dark.class.css';
 import "driver.js/dist/driver.css";
 
 import NotFound from './app/NotFound';
-
 import Background from './template/background';
-
 import Layout from './app/layout';
 import LayoutProductos from './app/productos/layout';
 import LayoutCarrito from './app/carrito/layout';
@@ -29,7 +27,6 @@ import LayoutSeguimiento from './app/seguimiento/layout';
 import LayoutUsuario from './app/perfil/layout';
 import LayoutPrivacidad from './app/privacidad/layout';
 import Tabs from './template/tabs';
-import { IonReactRouter } from '@ionic/react-router';
 import { usePushNotifications } from './hooks/use-notifications';
 import AccesDenied from './app/AccesDenied';
 
@@ -41,34 +38,33 @@ setupIonicReact({
 });
 
 const App: React.FC = () => {
- usePushNotifications();
+  usePushNotifications();
+
   return (
     <IonApp>
-      <IonReactRouter>
-        <IonTabs>
-          <IonRouterOutlet>
-            <Background>
-              <Switch>
-                <Route path="/home" render={() => <Layout />} exact />
-                <Route path="/privacidad" render={() => <LayoutPrivacidad />} exact />
-                <Route path="/productos" render={() => <LayoutProductos />} exact />
-                <Route path="/ofertas" render={() => <LayoutProductos />} exact />
-                <Route path="/carrito" render={() => <LayoutCarrito />} exact />
-                <Route path="/checkout" render={() => <LayoutCheckout />} exact />
-                <Route path="/seguimiento" render={() => <LayoutSeguimiento />} exact />
-                <Route path="/cuentas" render={() => <LayoutUsuario />} exact />
-                <Route path="/acceso-denegado" render={() => <AccesDenied />} exact />
-                <Redirect exact path="/" to="/home" />
-
-                <Route>
-                  <NotFound />
-                </Route>
-              </Switch>
-            </Background>
-          </IonRouterOutlet>
-          <Tabs />
-        </IonTabs>
-      </IonReactRouter>
+      {/* Eliminado el IonReactRouter anidado */}
+      <IonTabs>
+        <IonRouterOutlet>
+          <Background>
+            <Switch>
+              <Route path="/home" render={() => <Layout />} exact />
+              <Route path="/privacidad" render={() => <LayoutPrivacidad />} exact />
+              <Route path="/productos" render={() => <LayoutProductos />} exact />
+              <Route path="/ofertas" render={() => <LayoutProductos />} exact />
+              <Route path="/carrito" render={() => <LayoutCarrito />} exact />
+              <Route path="/checkout" render={() => <LayoutCheckout />} exact />
+              <Route path="/seguimiento" render={() => <LayoutSeguimiento />} exact />
+              <Route path="/cuentas" render={() => <LayoutUsuario />} exact />
+              <Route path="/acceso-denegado" render={() => <AccesDenied />} exact />
+              <Redirect exact path="/" to="/home" />
+              <Route>
+                <NotFound />
+              </Route>
+            </Switch>
+          </Background>
+        </IonRouterOutlet>
+        <Tabs />
+      </IonTabs>
     </IonApp>
   );
 };
